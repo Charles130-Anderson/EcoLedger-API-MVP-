d4b379206f62ea1e2b7ec57202fb5f27fb638895
Otieno Charles Anderson <138807102+Charles130-Anderson@users.noreply.github.com>
2025-06-21 09:58:02 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.routes import credits
-from app.database import engine, Base
+#from app.routes import credits
+#from app.database import engine, Base
 
 # ✅ Register models before calling create_all()
-Base.metadata.create_all(bind=engine)
+#Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="EcoLedger MVP API",
@@ -11,7 +11,7 @@ app = FastAPI(
     description="API for managing carbon credit records"
 )
 
-app.include_router(credits.router)
+#app.include_router(credits.router)
 
 @app.get("/")
 def root():
